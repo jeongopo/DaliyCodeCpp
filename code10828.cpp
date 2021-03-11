0a9ad28c827991de3576d04712f54b744122d3aa
jeongopo <jeong_opo@naver.com>
2021-03-12 00:03:10 +0900

    괄호

---
+++ code10828.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+void solution() {
+	stack<int> arr;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	string tem;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> tem;
+		if (tem == "push") {
+			int num;
+			cin >> num;
+			arr.push(num);
+		}
+		else if (tem == "pop") {
+			if (!arr.empty()) {
+				cout <<arr.top() << "\n";
+				arr.pop();
+			}
+			else cout << -1<<"\n";
+		}
+		else if (tem == "size") {
+			cout <<arr.size() << "\n";
+		}
+		else if (tem == "empty") {
+			cout <<arr.empty() << "\n";
+		}
+		else if (tem == "top") {
+			if (!arr.empty()) {
+				cout <<arr.top() << "\n";
+			}
+			else cout << -1<<"\n";
+		}
+	}
+}
+
+int main() {
+	solution();
+	return 0;
+}

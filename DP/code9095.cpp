34257f40e1747a39e70042456d10047d0a9afa7e
Hyun2and <52280545+jeongopo@users.noreply.github.com>
2021-08-27 16:07:47 +0900

    1,2,3 더하기

---
+++ DP/code9095.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+	int DP[12] = { 0,1,2,4, };
+	int ret[10];
+	int i,j;
+	int t = 0;
+	int n = 0;
+
+	for (i = 4; i <= 11; i++) {
+		DP[i] = DP[i - 1] + DP[i - 2] + DP[i - 3];
+	}
+
+	std::cin >> t;
+
+	for (j = 0; j < t; j++) { //t번의 테스트케이스 실행
+		std::cin >> n;
+		ret[j] = DP[n];
+	}
+	for (i = 0; i < t; i++) {
+		std::cout << ret[i] << "\n";
+	}
+	
+}
